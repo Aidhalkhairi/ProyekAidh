f9804d528a386c4838e3cce196d8f9c251e251c7
Aidhalkhairi <2300016131@webmail.uad.ac.id>
2024-12-20 19:49:03 +0700

    Add files via upload

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCeCHJJvuOAGHbXktQwFila5znFkta-lec",
+    authDomain: "proyek-aidh.firebaseapp.com",
+    projectId: "proyek-aidh",
+    storageBucket: "proyek-aidh.firebasestorage.app",
+    messagingSenderId: "913289999121",
+    appId: "1:913289999121:web:fb29b1d1fb01ac169c92b0"
+  };
+
+  const app = firebase.initializingApp(firebaseConfig);
